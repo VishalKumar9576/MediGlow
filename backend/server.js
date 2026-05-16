a81f5ef7a2e085a259c9607ef64c943da407f9a0
vishal <vishalks9576@gmail.com>
2026-05-17 02:57:15 +0530

    add root route

--- backend/server.js
+++ backend/server.js
@@ -64,6 +64,13 @@ app.get('/', (req, res) => {
 
 const PORT = Number(process.env.PORT) || 5000;
 
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Backend Running"
+  });
+});
+
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
